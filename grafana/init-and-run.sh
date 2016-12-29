7357a71f89c2b7f73944933b1a62066f81c582a1
Konrad Kurdej <konrad.kurdej@intel.com>
2016-12-29 15:53:47 +0100

    Safe and no auth initialization of Grafana

--- grafana/init-and-run.sh
+++ grafana/init-and-run.sh
@@ -16,65 +16,18 @@
 #
 
 
-GRAFANA_URL=localhost:3000
 INIT_FLAT_FILE=/grafana-data/.initialized
-
-function addFromFolder {
-
-    FOLDER=$1
-
-    echo "Adding datasources from $FOLDER"
-
-    for file in $FOLDER/*-datasource.json ; do
-      if [ -e "$file" ] ; then
-        echo "Adding datasource: $file"
-        curl --silent --fail --show-error \
-          -X POST http://$GRAFANA_URL/api/datasources \
-          -H "Content-Type: application/json;charset=UTF-8" \
-          -d @$file
-      fi
-    done
-
-    echo "Adding dashboards from $FOLDER"
-
-    for file in $FOLDER/*-dashboard.json ; do
-      if [ -e "$file" ] ; then
-        echo "Adding dashboard: $file"
-        curl --silent --fail --show-error \
-          -X POST http://$GRAFANA_URL/api/dashboards/import \
-          -H "Content-Type: application/json;charset=UTF-8" \
-          -d @$file
-      fi
-    done
-}
-
-
-echo "Starting Grafana"
-
-/run.sh &
-
-echo "Waiting for Grafana becomming available"
-
-
-until $(curl --silent --fail --show-error --output /dev/null http://$GRAFANA_URL/api/datasources); do
-  printf '.'
-  sleep 1
-done
 
 echo "Grafana available"
 
 if [ -e "$INIT_FLAT_FILE" ] ; then
     echo "Grafana already initialized"
 else
-    echo "Initializing static conent"
-    addFromFolder /grafana_init_static_content
-
-    echo "Initializing dynamic conent"
-    addFromFolder /grafana_init_dynamic_content
-
-    echo "Initialization compleated"
+    echo "Grafana is being initialized"
+    /init.sh
     touch $INIT_FLAT_FILE
 fi
 
-wait
+echo "Starting Grafana"
 
+exec /run.sh
---
+++ grafana/init.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+env -i bash
+
+GRAFANA_URL=localhost:3000
+DATA_DIR=/grafana-data
+
+
+function addFromFolder {
+
+    FOLDER=$1
+
+    echo "Adding datasources from $FOLDER"
+
+    for file in $FOLDER/*-datasource.json ; do
+      if [ -e "$file" ] ; then
+        echo "Adding datasource: $file"
+        curl --silent --fail --show-error \
+          -X POST http://$GRAFANA_URL/api/datasources \
+          -H "Content-Type: application/json;charset=UTF-8" \
+          -d @$file
+      fi
+    done
+
+    echo "Adding dashboards from $FOLDER"
+
+    for file in $FOLDER/*-dashboard.json ; do
+      if [ -e "$file" ] ; then
+        echo "Adding dashboard: $file"
+        curl --silent --fail --show-error \
+          -X POST http://$GRAFANA_URL/api/dashboards/import \
+          -H "Content-Type: application/json;charset=UTF-8" \
+          -d @$file
+      fi
+    done
+}
+
+
+echo "Starting local Grafana"
+
+GF_SERVER_HTTP_ADDR="127.0.0.1" \
+GF_AUTH_BASIC_ENABLED=false \
+GF_AUTH_ANONYMOUS_ENABLED=true \
+GF_AUTH_ANONYMOUS_ORG_ROLE=Admin \
+GF_PATHS_DATA=$DATA_DIR \
+/run.sh &
+
+
+echo "Waiting for Grafana becomming available"
+
+until $(curl --silent --fail --show-error --output /dev/null http://$GRAFANA_URL/api/datasources); do
+  printf '.'
+  sleep 1
+done
+
+echo "Grafana available"
+
+
+echo "Initializing static conent"
+addFromFolder /grafana_init_static_content
+
+echo "Initializing dynamic conent"
+addFromFolder /grafana_init_dynamic_content
+
+echo "Initialization compleated"
+
+
+echo "Killing internal Grafana"
+kill $(jobs -p)
+wait
+echo "Done - Grafana down"
+
